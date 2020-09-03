b7ee19dd7063798fb2b7f4cbfab54086570ba43f
azuline <azuline@riseup.net>
2020-09-03 15:11:37 -0400

    fix backend for 3.6

--- backend/util.py
+++ backend/util.py
@@ -14,7 +14,7 @@ punctuation = set(punctuation)
 @contextmanager
 def database() -> ContextManager[sqlite3.Connection]:
     """A simple wrapper for the sqlite3 connection context manager."""
-    with sqlite3.connect(DATABASE_PATH) as conn:
+    with sqlite3.connect(str(DATABASE_PATH)) as conn:
         conn.row_factory = sqlite3.Row
         conn.execute("PRAGMA foreign_keys = ON")
         yield conn
