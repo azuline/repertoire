a4bd976af1c955b8237e2fa0f32b17949ce8e9fe
azuline <azuline@riseup.net>
2020-11-13 13:02:10 -0500

    remove storybook from frontend

--- frontend/.storybook/decorators/withTailwind.tsx
+++
@@ -1,4 +0,0 @@
-import * as React from 'react';
-import 'src/index.css';
-
-export const withTailwind = (story) => <div className="bg-gray-100 p-8">{story()}</div>;
--- frontend/.storybook/decorators/withToastProvider.tsx
+++
@@ -1,6 +0,0 @@
-import { ToastProvider } from 'src/contexts';
-import * as React from 'react';
-
-export const withToastProvider = (story) => {
-  return <ToastProvider>{story()}</ToastProvider>;
-};
