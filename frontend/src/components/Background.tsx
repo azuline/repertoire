100a7fbbf410aa07744b8596ae891963e3f5eb70
azuline <azuline@riseup.net>
2020-11-27 21:21:02 -0500

    update background gradient

--- frontend/src/components/Background.tsx
+++ frontend/src/components/Background.tsx
@@ -5,7 +5,7 @@ import { BackgroundContext } from 'src/contexts';
 // TODO: Light theme-ify support this.
 const backgroundStyle = {
   background:
-    'linear-gradient(185deg, rgba(16, 16, 19, 0.5), rgba(16, 16, 19, 0.6), rgba(16, 16, 19, 0.8), rgba(16, 16, 19, 1), rgba(16, 16, 19, 1))',
+    'linear-gradient(190deg, rgba(16, 16, 19, 0.5), rgba(16, 16, 19, 0.6), rgba(16, 16, 19, 0.8), rgba(16, 16, 19, 1), rgba(16, 16, 19, 1))',
 };
 
 export const Background: React.FC = () => {
@@ -14,11 +14,11 @@ export const Background: React.FC = () => {
   if (backgroundImageId === null) return null;
 
   return (
-    <div className="absolute top-0 left-0 h-0 full">
-      <div className="absolute top-0 left-0 z-0 opacity-50 full">
-        <Image className="object-cover full" imageId={backgroundImageId} />
+    <div className="absolute top-0 left-0 w-full h-0">
+      <div className="absolute top-0 left-0 z-0 w-full h-0 opacity-50 pb-full">
+        <Image className="object-cover w-full" imageId={backgroundImageId} />
+        <div className="absolute top-0 left-0 z-0 max-h-screen full" style={backgroundStyle} />
       </div>
-      <div className="absolute top-0 left-0 z-0 max-h-screen full" style={backgroundStyle} />
     </div>
   );
 };
