d590618449fa38ff5a90fc1ed35ff07d3ff3e562
blissful <blissful@sunsetglow.net>
2021-02-11 22:53:56 -0500

    update lint-staged config

--- frontend/.lintstagedrc.js
+++ frontend/.lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   '*.{js,jsx,ts,tsx}': ['eslint --fix'],
   '*.{json,html,md}': ['prettier --write'],
-  '*.{py}': ['black && isort && flake8'],
+  '*.py': ['black', 'isort', 'flake8'],
 };
