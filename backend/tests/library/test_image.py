d17d8c3fa8ce097cff9c97c6f4f746250ff2f902
blissful <blissful@sunsetglow.net>
2021-02-27 15:36:36 -0500

    fix broken image tests

--- backend/tests/library/test_image.py
+++ backend/tests/library/test_image.py
@@ -1,5 +1,7 @@
 import shutil
 from pathlib import Path
+from tests.factory import Factory
+from sqlite3 import Connection
 
 import pytest
 from PIL import Image
@@ -11,48 +13,43 @@ from tests.conftest import NEXT_IMAGE_ID, SEED_DATA
 FAKE_COVER = SEED_DATA / "fake_cover.jpg"
 
 
-def test_from_id(db, snapshot):
-    snapshot.assert_match(image.from_id(1, db))
+def test_from_id(factory: Factory, db: Connection):
+    img = factory.mock_image(conn=db)
+    new_img = image.from_id(img.id, db)
+    assert img == new_img
 
 
-def test_from_id_nonexistent(db):
+def test_from_id_nonexistent(db: Connection):
     assert image.from_id(999, db) is None
 
 
-def test_from_path(db, snapshot):
-    db.execute("UPDATE images SET path = '/lol' WHERE id = 1")
+def test_from_path(factory: Factory, db: Connection):
+    img = factory.mock_image(path=Path("/lol"), conn=db)
+    assert img == image.from_path("/lol", db)
 
-    assert image.from_path("/lol", db).id == 1
 
-
-def test_from_path_nonexistent(db, snapshot):
+def test_from_path_nonexistent(db: Connection):
     assert image.from_path("/lol", db) is None
 
 
-def test_create(db):
-    image_path = Path.cwd() / "cover.jpg"
-    shutil.copyfile(FAKE_COVER, image_path)
-    img = image.create(image_path, db)
-
-    assert img.id == NEXT_IMAGE_ID
-    assert img.path == image_path
-    assert img == image.from_id(NEXT_IMAGE_ID, db)
+def test_create(factory: Factory, db: Connection):
+    img = factory.image(conn=db)
 
     # Check thumbnail was generated.
-    im = Image.open(Path.cwd() / "cover.thumbnail")
+    im = Image.open(img.path.with_suffix(".thumbnail"))
     assert im.size == (300, 300)
 
 
-def test_create_duplicate(db):
-    db.execute("UPDATE images SET path = '/lol' WHERE id = 1")
+def test_create_duplicate(factory: Factory, db: Connection):
+    img = factory.mock_image(conn=db)
 
     with pytest.raises(Duplicate) as e:
-        image.create("/lol", db)
+        image.create(img.path, db)
 
-    assert e.value.entity.id == 1
+    assert e.value.entity == img
 
 
-def test_create_invalid_image(db):
+def test_create_invalid_image(factory: Factory, db: Connection):
     image_path = Path.cwd() / "cover.jpg"
     with image_path.open("wb") as f:
         f.write(b"not an image lol!")
@@ -60,39 +57,28 @@ def test_create_invalid_image(db):
     with pytest.raises(InvalidImage):
         image.create(image_path, db)
 
-    assert image.from_id(NEXT_IMAGE_ID, db) is None
     assert not image_path.exists()
 
 
-def test_delete_image(db):
+def test_delete_image(factory: Factory, db: Connection):
     image_path = Path.cwd() / "cover.jpg"
     thumbnail_path = Path.cwd() / "cover.thumbnail"
     image_path.touch()
     thumbnail_path.touch()
 
-    cursor = db.execute("INSERT INTO images (path) VALUES (?)", (str(image_path),))
-    image_id = cursor.lastrowid
-
-    img = image.from_id(image_id, db)
-    assert img is not None
+    img = factory.mock_image(path=image_path, conn=db)
     image.delete(img, db)
 
-    assert not image.from_id(image_id, db)
+    assert not image.from_id(img.id, db)
     assert not image_path.exists()
     assert not thumbnail_path.exists()
 
 
-def test_delete_image_files_missing(db):
-    # Check to see it doesn't error when the files are missing.
+def test_delete_image_files_missing(factory: Factory, db: Connection):
     image_path = Path.cwd() / "cover.jpg"
-    cursor = db.execute("INSERT INTO images (path) VALUES (?)", (str(image_path),))
-    image_id = cursor.lastrowid
-
-    img = image.from_id(image_id, db)
-    assert img is not None
+    img = factory.mock_image(path=image_path, conn=db)
     image.delete(img, db)
-
-    assert not image.from_id(image_id, db)
+    assert not image.from_id(img.id, db)
 
 
 def test_thumbnail_path():
