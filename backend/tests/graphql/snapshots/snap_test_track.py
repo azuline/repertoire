bd9f10f71d9eabc92c613b7d18a9cd5267c245e8
blissful <blissful@sunsetglow.net>
2021-02-28 14:45:59 -0500

    fix track gql tests

--- backend/tests/graphql/snapshots/snap_test_track.py
+++ backend/tests/graphql/snapshots/snap_test_track.py
@@ -2,7 +2,7 @@
 # snapshottest: v1 - https://goo.gl/zC4yUc
 from __future__ import unicode_literals
 
-from snapshottest import GenericRepr, Snapshot
+from snapshottest import Snapshot
 
 
 snapshots = Snapshot()
@@ -24,14 +24,14 @@ snapshots['test_add_artist_to_track 1'] = {
                             'id': 3,
                             'name': 'Artist2'
                         },
-                        'role': 'MAIN'
+                        'role': 'PRODUCER'
                     },
                     {
                         'artist': {
-                            'id': 3,
-                            'name': 'Artist2'
+                            'id': 5,
+                            'name': 'Artist4'
                         },
-                        'role': 'PRODUCER'
+                        'role': 'MAIN'
                     }
                 ],
                 'discNumber': '1',
@@ -46,17 +46,13 @@ snapshots['test_add_artist_to_track 1'] = {
             },
             'trackArtist': {
                 'artist': {
-                    'id': 3,
-                    'name': 'Artist2',
-                    'numReleases': 2,
+                    'id': 5,
+                    'name': 'Artist4',
+                    'numReleases': 1,
                     'releases': [
                         {
-                            'id': 2,
-                            'title': 'Release1'
-                        },
-                        {
-                            'id': 3,
-                            'title': 'Release2'
+                            'id': 4,
+                            'title': 'Release3'
                         }
                     ],
                     'starred': False,
@@ -65,13 +61,13 @@ snapshots['test_add_artist_to_track 1'] = {
                             'genre': {
                                 'id': 9
                             },
-                            'numMatches': 2
+                            'numMatches': 1
                         },
                         {
                             'genre': {
                                 'id': 10
                             },
-                            'numMatches': 2
+                            'numMatches': 1
                         }
                     ]
                 },
@@ -80,21 +76,6 @@ snapshots['test_add_artist_to_track 1'] = {
         }
     }
 }
-
-snapshots['test_add_artist_to_track 2'] = [
-    {
-        'artist': GenericRepr("T(id=2, name='Artist1', starred=False, num_releases=2)"),
-        'role': GenericRepr('<ArtistRoles.MAIN: 1>')
-    },
-    {
-        'artist': GenericRepr("T(id=3, name='Artist2', starred=False, num_releases=2)"),
-        'role': GenericRepr('<ArtistRoles.MAIN: 1>')
-    },
-    {
-        'artist': GenericRepr("T(id=3, name='Artist2', starred=False, num_releases=2)"),
-        'role': GenericRepr('<ArtistRoles.PRODUCER: 4>')
-    }
-]
 
 snapshots['test_add_artist_to_track_already_exists 1'] = {
     'data': {
@@ -117,17 +98,6 @@ snapshots['test_add_artist_to_track_already_exists 1'] = {
     ]
 }
 
-snapshots['test_add_artist_to_track_already_exists 2'] = [
-    {
-        'artist': GenericRepr("T(id=2, name='Artist1', starred=False, num_releases=2)"),
-        'role': GenericRepr('<ArtistRoles.MAIN: 1>')
-    },
-    {
-        'artist': GenericRepr("T(id=3, name='Artist2', starred=False, num_releases=2)"),
-        'role': GenericRepr('<ArtistRoles.PRODUCER: 4>')
-    }
-]
-
 snapshots['test_add_artist_to_track_bad_artist 1'] = {
     'data': {
         'addArtistToTrack': None
@@ -148,17 +118,6 @@ snapshots['test_add_artist_to_track_bad_artist 1'] = {
         }
     ]
 }
-
-snapshots['test_add_artist_to_track_bad_artist 2'] = [
-    {
-        'artist': GenericRepr("T(id=2, name='Artist1', starred=False, num_releases=2)"),
-        'role': GenericRepr('<ArtistRoles.MAIN: 1>')
-    },
-    {
-        'artist': GenericRepr("T(id=3, name='Artist2', starred=False, num_releases=2)"),
-        'role': GenericRepr('<ArtistRoles.PRODUCER: 4>')
-    }
-]
 
 snapshots['test_add_artist_to_track_bad_track 1'] = {
     'data': {
@@ -247,13 +206,6 @@ snapshots['test_del_artist_from_track 1'] = {
     }
 }
 
-snapshots['test_del_artist_from_track 2'] = [
-    {
-        'artist': GenericRepr("T(id=3, name='Artist2', starred=False, num_releases=2)"),
-        'role': GenericRepr('<ArtistRoles.PRODUCER: 4>')
-    }
-]
-
 snapshots['test_del_artist_from_track_bad_artist 1'] = {
     'data': {
         'delArtistFromTrack': None
@@ -274,17 +226,6 @@ snapshots['test_del_artist_from_track_bad_artist 1'] = {
         }
     ]
 }
-
-snapshots['test_del_artist_from_track_bad_artist 2'] = [
-    {
-        'artist': GenericRepr("T(id=2, name='Artist1', starred=False, num_releases=2)"),
-        'role': GenericRepr('<ArtistRoles.MAIN: 1>')
-    },
-    {
-        'artist': GenericRepr("T(id=3, name='Artist2', starred=False, num_releases=2)"),
-        'role': GenericRepr('<ArtistRoles.PRODUCER: 4>')
-    }
-]
 
 snapshots['test_del_artist_from_track_bad_track 1'] = {
     'data': {
@@ -341,28 +282,21 @@ snapshots['test_track 1'] = {
                 },
                 {
                     'artist': {
-                        'id': 4,
-                        'name': 'Artist3'
+                        'id': 3,
+                        'name': 'Artist2'
                     },
-                    'role': 'FEATURE'
-                },
-                {
-                    'artist': {
-                        'id': 6,
-                        'name': 'Artist5'
-                    },
-                    'role': 'REMIXER'
+                    'role': 'PRODUCER'
                 }
             ],
             'discNumber': '1',
-            'duration': 9,
-            'id': 10,
+            'duration': 0,
+            'id': 1,
             'release': {
-                'id': 5,
-                'title': 'Release4'
+                'id': 2,
+                'title': 'Release1'
             },
-            'title': 'Track9',
-            'trackNumber': '9'
+            'title': 'Track0',
+            'trackNumber': '0'
         }
     }
 }
@@ -1054,8 +988,143 @@ snapshots['test_tracks_filter_playlists 1'] = {
     'data': {
         'tracks': {
             'results': [
+                {
+                    'artists': [
+                        {
+                            'artist': {
+                                'id': 2,
+                                'name': 'Artist1'
+                            },
+                            'role': 'MAIN'
+                        },
+                        {
+                            'artist': {
+                                'id': 3,
+                                'name': 'Artist2'
+                            },
+                            'role': 'PRODUCER'
+                        }
+                    ],
+                    'discNumber': '1',
+                    'duration': 0,
+                    'id': 1,
+                    'release': {
+                        'id': 2,
+                        'title': 'Release1'
+                    },
+                    'title': 'Track0',
+                    'trackNumber': '0'
+                },
+                {
+                    'artists': [
+                        {
+                            'artist': {
+                                'id': 2,
+                                'name': 'Artist1'
+                            },
+                            'role': 'MAIN'
+                        },
+                        {
+                            'artist': {
+                                'id': 3,
+                                'name': 'Artist2'
+                            },
+                            'role': 'PRODUCER'
+                        }
+                    ],
+                    'discNumber': '1',
+                    'duration': 1,
+                    'id': 2,
+                    'release': {
+                        'id': 2,
+                        'title': 'Release1'
+                    },
+                    'title': 'Track1',
+                    'trackNumber': '1'
+                },
+                {
+                    'artists': [
+                        {
+                            'artist': {
+                                'id': 2,
+                                'name': 'Artist1'
+                            },
+                            'role': 'MAIN'
+                        },
+                        {
+                            'artist': {
+                                'id': 3,
+                                'name': 'Artist2'
+                            },
+                            'role': 'PRODUCER'
+                        }
+                    ],
+                    'discNumber': '1',
+                    'duration': 2,
+                    'id': 3,
+                    'release': {
+                        'id': 2,
+                        'title': 'Release1'
+                    },
+                    'title': 'Track2',
+                    'trackNumber': '2'
+                },
+                {
+                    'artists': [
+                        {
+                            'artist': {
+                                'id': 3,
+                                'name': 'Artist2'
+                            },
+                            'role': 'MAIN'
+                        },
+                        {
+                            'artist': {
+                                'id': 4,
+                                'name': 'Artist3'
+                            },
+                            'role': 'FEATURE'
+                        }
+                    ],
+                    'discNumber': '3',
+                    'duration': 3,
+                    'id': 4,
+                    'release': {
+                        'id': 3,
+                        'title': 'Release2'
+                    },
+                    'title': 'Track3',
+                    'trackNumber': '3'
+                },
+                {
+                    'artists': [
+                        {
+                            'artist': {
+                                'id': 3,
+                                'name': 'Artist2'
+                            },
+                            'role': 'MAIN'
+                        },
+                        {
+                            'artist': {
+                                'id': 4,
+                                'name': 'Artist3'
+                            },
+                            'role': 'FEATURE'
+                        }
+                    ],
+                    'discNumber': '4',
+                    'duration': 4,
+                    'id': 5,
+                    'release': {
+                        'id': 3,
+                        'title': 'Release2'
+                    },
+                    'title': 'Track4',
+                    'trackNumber': '4'
+                }
             ],
-            'total': 0
+            'total': 5
         }
     }
 }
@@ -1128,8 +1197,163 @@ snapshots['test_tracks_search 1'] = {
     'data': {
         'tracks': {
             'results': [
+                {
+                    'artists': [
+                    ],
+                    'discNumber': '13',
+                    'duration': 13,
+                    'id': 14,
+                    'release': {
+                        'id': 6,
+                        'title': 'Release5'
+                    },
+                    'title': 'Track13',
+                    'trackNumber': '13'
+                },
+                {
+                    'artists': [
+                    ],
+                    'discNumber': '14',
+                    'duration': 14,
+                    'id': 15,
+                    'release': {
+                        'id': 6,
+                        'title': 'Release5'
+                    },
+                    'title': 'Track14',
+                    'trackNumber': '14'
+                },
+                {
+                    'artists': [
+                        {
+                            'artist': {
+                                'id': 2,
+                                'name': 'Artist1'
+                            },
+                            'role': 'MAIN'
+                        },
+                        {
+                            'artist': {
+                                'id': 3,
+                                'name': 'Artist2'
+                            },
+                            'role': 'PRODUCER'
+                        }
+                    ],
+                    'discNumber': '1',
+                    'duration': 1,
+                    'id': 2,
+                    'release': {
+                        'id': 2,
+                        'title': 'Release1'
+                    },
+                    'title': 'Track1',
+                    'trackNumber': '1'
+                },
+                {
+                    'artists': [
+                        {
+                            'artist': {
+                                'id': 2,
+                                'name': 'Artist1'
+                            },
+                            'role': 'MAIN'
+                        },
+                        {
+                            'artist': {
+                                'id': 4,
+                                'name': 'Artist3'
+                            },
+                            'role': 'FEATURE'
+                        },
+                        {
+                            'artist': {
+                                'id': 6,
+                                'name': 'Artist5'
+                            },
+                            'role': 'REMIXER'
+                        }
+                    ],
+                    'discNumber': '1',
+                    'duration': 10,
+                    'id': 11,
+                    'release': {
+                        'id': 5,
+                        'title': 'Release4'
+                    },
+                    'title': 'Track10',
+                    'trackNumber': '10'
+                },
+                {
+                    'artists': [
+                        {
+                            'artist': {
+                                'id': 2,
+                                'name': 'Artist1'
+                            },
+                            'role': 'MAIN'
+                        },
+                        {
+                            'artist': {
+                                'id': 4,
+                                'name': 'Artist3'
+                            },
+                            'role': 'FEATURE'
+                        },
+                        {
+                            'artist': {
+                                'id': 6,
+                                'name': 'Artist5'
+                            },
+                            'role': 'REMIXER'
+                        }
+                    ],
+                    'discNumber': '1',
+                    'duration': 11,
+                    'id': 12,
+                    'release': {
+                        'id': 5,
+                        'title': 'Release4'
+                    },
+                    'title': 'Track11',
+                    'trackNumber': '11'
+                },
+                {
+                    'artists': [
+                        {
+                            'artist': {
+                                'id': 2,
+                                'name': 'Artist1'
+                            },
+                            'role': 'MAIN'
+                        },
+                        {
+                            'artist': {
+                                'id': 4,
+                                'name': 'Artist3'
+                            },
+                            'role': 'FEATURE'
+                        },
+                        {
+                            'artist': {
+                                'id': 6,
+                                'name': 'Artist5'
+                            },
+                            'role': 'REMIXER'
+                        }
+                    ],
+                    'discNumber': '1',
+                    'duration': 12,
+                    'id': 13,
+                    'release': {
+                        'id': 5,
+                        'title': 'Release4'
+                    },
+                    'title': 'Track12',
+                    'trackNumber': '12'
+                }
             ],
-            'total': 0
+            'total': 6
         }
     }
 }
@@ -1968,8 +2192,6 @@ snapshots['test_update_track 1'] = {
     }
 }
 
-snapshots['test_update_track 2'] = GenericRepr("T(id=2, filepath=PosixPath('/home/azul/devel/repertoire/backend/music/track2.flac'), sha256=b'\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01', title='aa', release_id=3, duration=1, track_number='999', disc_number='899')")
-
 snapshots['test_update_track_bad_release_id 1'] = {
     'data': {
         'updateTrack': None
@@ -1990,8 +2212,6 @@ snapshots['test_update_track_bad_release_id 1'] = {
         }
     ]
 }
-
-snapshots['test_update_track_bad_release_id 2'] = GenericRepr("T(id=2, filepath=PosixPath('/home/azul/devel/repertoire/backend/music/track2.flac'), sha256=b'\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01\\x01', title='Track1', release_id=2, duration=1, track_number='1', disc_number='1')")
 
 snapshots['test_update_track_not_found 1'] = {
     'data': {
--- backend/tests/graphql/test_track.py
+++ backend/tests/graphql/test_track.py
@@ -1,5 +1,8 @@
+from sqlite3 import Connection
+
 import pytest
 
+from src.library import playlist_entry as pentry
 from src.library import track
 
 
@@ -7,7 +10,7 @@ from src.library import track
 async def test_track(graphql_query, snapshot):
     query = """
         query {
-            track(id: 10) {
+            track(id: 1) {
                 ...TrackFields
             }
         }
@@ -32,7 +35,7 @@ async def test_track_not_found(graphql_query, snapshot):
 
 
 @pytest.mark.asyncio
-async def test_track_favorited(graphql_query, snapshot):
+async def test_track_favorited(db: Connection, graphql_query, snapshot):
     query = """
         query {
             track(id: 1) {
@@ -40,21 +43,28 @@ async def test_track_favorited(graphql_query, snapshot):
             }
         }
     """
-    _, res = await graphql_query(query)
-    assert res["data"]["track"]["favorited"]
+
+    # Setup
+    pentry.create(1, 1, db)
+    db.commit()
+
+    success, data = await graphql_query(query)
+    assert success is True
+    assert data["data"]["track"]["favorited"]
 
 
 @pytest.mark.asyncio
 async def test_track_favorited_false(graphql_query, snapshot):
     query = """
         query {
-            track(id: 6) {
+            track(id: 1) {
                 favorited
             }
         }
     """
-    _, res = await graphql_query(query)
-    assert not res["data"]["track"]["favorited"]
+    success, data = await graphql_query(query)
+    assert success is True
+    assert not data["data"]["track"]["favorited"]
 
 
 @pytest.mark.asyncio
@@ -78,7 +88,7 @@ async def test_tracks(graphql_query, snapshot):
 async def test_tracks_search(graphql_query, snapshot):
     query = """
         query {
-            tracks(search: "Aaron") {
+            tracks(search: "Track1") {
                 total
                 results {
                     ...TrackFields
@@ -95,7 +105,7 @@ async def test_tracks_search(graphql_query, snapshot):
 async def test_tracks_filter_playlists(graphql_query, snapshot):
     query = """
         query {
-            tracks(playlistIds: [1]) {
+            tracks(playlistIds: [2]) {
                 total
                 results {
                     ...TrackFields
@@ -177,7 +187,7 @@ async def test_tracks_sort_desc(graphql_query, snapshot):
 
 
 @pytest.mark.asyncio
-async def test_update_track(db, graphql_query, snapshot):
+async def test_update_track(db: Connection, graphql_query, snapshot):
     query = """
         mutation {
             updateTrack(
@@ -194,11 +204,17 @@ async def test_update_track(db, graphql_query, snapshot):
     success, data = await graphql_query(query)
     assert success is True
     snapshot.assert_match(data)
-    snapshot.assert_match(track.from_id(2, db))
+
+    trk = track.from_id(2, db)
+    assert trk is not None
+    assert trk.title == "aa"
+    assert trk.release_id == 3
+    assert trk.track_number == "999"
+    assert trk.disc_number == "899"
 
 
 @pytest.mark.asyncio
-async def test_update_track_bad_release_id(db, graphql_query, snapshot):
+async def test_update_track_bad_release_id(db: Connection, graphql_query, snapshot):
     query = """
         mutation {
             updateTrack(
@@ -212,7 +228,10 @@ async def test_update_track_bad_release_id(db, graphql_query, snapshot):
     success, data = await graphql_query(query)
     assert success is True
     snapshot.assert_match(data)
-    snapshot.assert_match(track.from_id(2, db))
+
+    trk = track.from_id(2, db)
+    assert trk is not None
+    assert trk.release_id != 999
 
 
 @pytest.mark.asyncio
@@ -233,10 +252,10 @@ async def test_update_track_not_found(graphql_query, snapshot):
 
 
 @pytest.mark.asyncio
-async def test_add_artist_to_track(db, graphql_query, snapshot):
+async def test_add_artist_to_track(db: Connection, graphql_query, snapshot):
     query = """
         mutation {
-            addArtistToTrack(trackId: 1, artistId: 3, role: MAIN) {
+            addArtistToTrack(trackId: 1, artistId: 5, role: MAIN) {
                 track {
                     ...TrackFields
                 }
@@ -252,9 +271,10 @@ async def test_add_artist_to_track(db, graphql_query, snapshot):
     success, data = await graphql_query(query)
     assert success is True
     snapshot.assert_match(data)
+
     trk = track.from_id(1, db)
     assert trk is not None
-    snapshot.assert_match(track.artists(trk, db))
+    assert 5 in [a["artist"].id for a in track.artists(trk, db)]
 
 
 @pytest.mark.asyncio
@@ -280,7 +300,7 @@ async def test_add_artist_to_track_bad_track(graphql_query, snapshot):
 
 
 @pytest.mark.asyncio
-async def test_add_artist_to_track_bad_artist(db, graphql_query, snapshot):
+async def test_add_artist_to_track_bad_artist(db: Connection, graphql_query, snapshot):
     query = """
         mutation {
             addArtistToTrack(trackId: 1, artistId: 9999, role: MAIN) {
@@ -296,16 +316,26 @@ async def test_add_artist_to_track_bad_artist(db, graphql_query, snapshot):
             }
         }
     """
+    trk = track.from_id(1, db)
+    assert trk is not None
+
+    before_artists = track.artists(trk, db)
+
     success, data = await graphql_query(query)
     assert success is True
     snapshot.assert_match(data)
-    trk = track.from_id(1, db)
-    assert trk is not None
-    snapshot.assert_match(track.artists(trk, db))
+
+    after_artists = track.artists(trk, db)
+
+    assert before_artists == after_artists
 
 
 @pytest.mark.asyncio
-async def test_add_artist_to_track_already_exists(db, graphql_query, snapshot):
+async def test_add_artist_to_track_already_exists(
+    db: Connection,
+    graphql_query,
+    snapshot,
+):
     query = """
         mutation {
             addArtistToTrack(trackId: 1, artistId: 2, role: MAIN) {
@@ -321,16 +351,22 @@ async def test_add_artist_to_track_already_exists(db, graphql_query, snapshot):
             }
         }
     """
+    trk = track.from_id(1, db)
+    assert trk is not None
+
+    before_artists = track.artists(trk, db)
+
     success, data = await graphql_query(query)
     assert success is True
     snapshot.assert_match(data)
-    trk = track.from_id(1, db)
-    assert trk is not None
-    snapshot.assert_match(track.artists(trk, db))
+
+    after_artists = track.artists(trk, db)
+
+    assert before_artists == after_artists
 
 
 @pytest.mark.asyncio
-async def test_del_artist_from_track(db, graphql_query, snapshot):
+async def test_del_artist_from_track(db: Connection, graphql_query, snapshot):
     query = """
         mutation {
             delArtistFromTrack(trackId: 1, artistId: 2, role: MAIN) {
@@ -349,9 +385,10 @@ async def test_del_artist_from_track(db, graphql_query, snapshot):
     success, data = await graphql_query(query)
     assert success is True
     snapshot.assert_match(data)
+
     trk = track.from_id(1, db)
     assert trk is not None
-    snapshot.assert_match(track.artists(trk, db))
+    assert 2 not in [a["artist"].id for a in track.artists(trk, db)]
 
 
 @pytest.mark.asyncio
@@ -377,7 +414,11 @@ async def test_del_artist_from_track_bad_track(graphql_query, snapshot):
 
 
 @pytest.mark.asyncio
-async def test_del_artist_from_track_bad_artist(db, graphql_query, snapshot):
+async def test_del_artist_from_track_bad_artist(
+    db: Connection,
+    graphql_query,
+    snapshot,
+):
     query = """
         mutation {
             delArtistFromTrack(trackId: 2, artistId: 9999, role: MAIN) {
@@ -393,16 +434,26 @@ async def test_del_artist_from_track_bad_artist(db, graphql_query, snapshot):
             }
         }
     """
+    trk = track.from_id(2, db)
+    assert trk is not None
+
+    before_artists = track.artists(trk, db)
+
     success, data = await graphql_query(query)
     assert success is True
     snapshot.assert_match(data)
-    trk = track.from_id(2, db)
-    assert trk is not None
-    snapshot.assert_match(track.artists(trk, db))
+
+    after_artists = track.artists(trk, db)
+
+    assert before_artists == after_artists
 
 
 @pytest.mark.asyncio
-async def test_del_artist_from_track_doesnt_exist(graphql_query, snapshot):
+async def test_del_artist_from_track_doesnt_exist(
+    db: Connection,
+    graphql_query,
+    snapshot,
+):
     query = """
         mutation {
             delArtistFromTrack(trackId: 1, artistId: 2, role: FEATURE) {
@@ -418,6 +469,15 @@ async def test_del_artist_from_track_doesnt_exist(graphql_query, snapshot):
             }
         }
     """
+    trk = track.from_id(1, db)
+    assert trk is not None
+
+    before_artists = track.artists(trk, db)
+
     success, data = await graphql_query(query)
     assert success is True
     snapshot.assert_match(data)
+
+    after_artists = track.artists(trk, db)
+
+    assert before_artists == after_artists
