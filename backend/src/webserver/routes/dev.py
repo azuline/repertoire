c081b8f4f2b0da261fd6c9444bb3956a1cda6ca4
blissful <blissful@sunsetglow.net>
2021-03-05 21:20:54 -0500

    add dev endpoint to index library in blocking request

--- backend/src/webserver/routes/dev.py
+++ backend/src/webserver/routes/dev.py
@@ -2,10 +2,11 @@ import quart
 from quart import Blueprint
 
 import logging
+from src.indexer import run_indexer
 
 logger = logging.getLogger(__name__)
 
-bp = Blueprint("dev", __name__, url_prefix="/dev")
+bp = Blueprint("dev", __name__, url_prefix="/api/dev")
 
 
 @bp.route("/testuser", methods=["POST"])
@@ -45,5 +46,24 @@ async def make_test_user() -> tuple[str, int]:
     )
 
     quart.g.db.commit()
+
+    return "success", 200
+
+
+@bp.route("/indexlib", methods=["POST"])
+async def index_library() -> tuple[str, int]:
+    """
+    **Developer endpoint.**
+
+    Indexes the library in a blocking request if we are in debug mode,
+    otherwise raises a 404.
+    """
+    if not quart.current_app.debug:
+        quart.abort(404)
+
+    logger.info("Indexing library in blocking request.")
+
+    # This better be under 30s...
+    run_indexer()
 
     return "success", 200
--- backend/tests/webserver/test_route_dev.py
+++ backend/tests/webserver/test_route_dev.py
@@ -11,7 +11,7 @@ from src.library import user
 async def test_create_dev_user(db: Connection, quart_app, quart_client):
     quart_app.debug = True
 
-    response = await quart_client.post("/dev/testuser")
+    response = await quart_client.post("/api/dev/testuser")
     assert response.status_code == 200
 
     usr = user.from_nickname("tester", db)
@@ -20,12 +20,22 @@ async def test_create_dev_user(db: Connection, quart_app, quart_client):
 
 
 @pytest.mark.asyncio
-async def test_create_dev_user_404(
-    factory: Factory,
-    db: Connection,
-    quart_app,
-    quart_client,
-):
+async def test_create_dev_user_404(db: Connection, quart_app, quart_client):
     quart_app.debug = False
-    response = await quart_client.post("/dev/testuser")
+    response = await quart_client.post("/api/dev/testuser")
+    assert response.status_code == 404
+
+
+@pytest.mark.asyncio
+async def test_dev_index_library(db: Connection, quart_app, quart_client):
+    quart_app.debug = True
+    response = await quart_client.post("/api/dev/indexlib")
+    # It just calls run_indexer, I'm not going to bother with a real test.
+    assert response.status_code == 200
+
+
+@pytest.mark.asyncio
+async def test_dev_index_library_404(quart_app, quart_client):
+    quart_app.debug = False
+    response = await quart_client.post("/api/dev/indexlib")
     assert response.status_code == 404
