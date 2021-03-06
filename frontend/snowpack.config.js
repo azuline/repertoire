20ce7711b67ca38ace874446a8d4e460d2185b06
blissful <blissful@sunsetglow.net>
2021-03-06 13:51:00 -0500

    catch errs

--- frontend/snowpack.config.js
+++ frontend/snowpack.config.js
@@ -4,6 +4,16 @@ const httpProxy = require('http-proxy');
 const backendHost = process.env.BACKEND_HOST ?? 'localhost';
 const proxy = httpProxy.createServer({ target: `http://${backendHost}:5000` });
 
+proxy.on('err', (err, req, res) => {
+  res.writeHead(500, {
+    'Content-Type': 'text/plain',
+  });
+
+  res.end('An error occurred with the request.');
+
+  console.log(`Error ${err} on request ${req}.`);
+});
+
 module.exports = {
   extends: '@snowpack/app-scripts-react',
   alias: {
@@ -13,8 +23,8 @@ module.exports = {
   },
   routes: [
     /* Proxy API calls. */
-    { src: '/api/.*', dest: (req, res) => proxyRequest(req, res) },
-    { src: '/graphql', dest: (req, res) => proxyRequest(req, res) },
+    { src: '/api/.*', dest: (req, res) => proxy.web(req, res) },
+    { src: '/graphql', dest: (req, res) => proxy.web(req, res) },
     /* Enable an SPA Fallback in development: */
     { match: 'routes', src: '.*', dest: '/index.html' },
   ],
@@ -31,12 +41,4 @@ module.exports = {
     output: 'stream',
   },
   buildOptions: {},
-};
-
-const proxyRequest = (req, res) => {
-  try {
-    proxy.web(req, res);
-  } catch (e) {
-    console.log(`Error: ${e}`);
-  }
 };
