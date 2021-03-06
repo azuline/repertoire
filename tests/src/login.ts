b2185888a40f726d903f64582428cc679b29f5af
blissful <blissful@sunsetglow.net>
2021-03-06 02:11:55 -0500

    does running in two sessions work??

--- tests/src/login.ts
+++ tests/src/login.ts
@@ -1,14 +1,18 @@
 Feature('login');
 
 Scenario('login step', ({ I }) => {
-  I.login();
+  session('authed', () => {
+    I.login();
+  });
 });
 
 Scenario('login failed', ({ I }) => {
-  I.amOnPage('/');
-  I.waitForElement('input');
-  I.fillField('Authorization token', '0101010101');
-  I.click('Login');
-  I.waitForElement('.react-toast-notifications__toast', 5);
-  I.see('Login failed.', { css: '.react-toast-notifications__toast' });
+  session('unauthed', () => {
+    I.amOnPage('/');
+    I.waitForElement('input');
+    I.fillField('Authorization token', '0101010101');
+    I.click('Login');
+    I.waitForElement('.react-toast-notifications__toast', 5);
+    I.see('Login failed.', { css: '.react-toast-notifications__toast' });
+  });
 });
