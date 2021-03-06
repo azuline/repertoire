3c896fb3de356a83ec5c33e833917a0cd5b8f5dc
blissful <blissful@sunsetglow.net>
2021-03-05 21:30:27 -0500

    create necessary shit as part of env setup

--- tests/codecept.conf.js
+++ tests/codecept.conf.js
@@ -1,5 +1,6 @@
 require('ts-node/register');
 
+const fetch = require('node-fetch');
 const { setHeadlessWhen } = require('@codeceptjs/configure');
 
 // turn on headless mode when running with HEADLESS=true environment variable
@@ -13,11 +14,11 @@ exports.config = {
     Playwright: {
       url: 'http://localhost:3000',
       show: true,
-      browser: 'chromium'
-    }
+      browser: 'chromium',
+    },
   },
   include: {
-    I: './steps_file.js'
+    I: './steps_file.js',
   },
   bootstrap: null,
   mocha: {},
@@ -25,13 +26,35 @@ exports.config = {
   plugins: {
     pauseOnFail: {},
     retryFailedStep: {
-      enabled: true
+      enabled: true,
     },
     tryTo: {
-      enabled: true
+      enabled: true,
     },
     screenshotOnFail: {
-      enabled: true
+      enabled: true,
+    },
+  },
+  bootstrap: async () => {
+    try {
+      const res1 = await fetch('http://localhost:3000/api/dev/testuser', {
+        method: 'POST',
+      });
+      if (res1.status !== 200) {
+        console.log('Failed to create test user.');
+        throw new Error('Failed to create test user.');
+      }
+
+      const res2 = await fetch('http://localhost:3000/api/dev/indexlib', {
+        method: 'POST',
+      });
+      if (res2.status !== 200) {
+        console.log('Failed to index library.');
+        throw new Error('Failed to index library.');
+      }
+    } catch (e) {
+      console.log(e);
+      throw e;
     }
-  }
-}
+  },
+};
