e69e367a39cd18b7c4382a84992cd243e7801bc0
blissful <blissful@sunsetglow.net>
2021-03-06 02:19:37 -0500

    remove sessions

--- tests/src/login.ts
+++ tests/src/login.ts
@@ -1,18 +1,14 @@
 Feature('login');
 
 Scenario('login step', ({ I }) => {
-  session('authed', () => {
-    I.login();
-  });
+  I.login();
 });
 
 Scenario('login failed', ({ I }) => {
-  session('unauthed', () => {
-    I.amOnPage('/');
-    I.waitForElement('input');
-    I.fillField('Authorization token', '0101010101');
-    I.click('Login');
-    I.waitForElement('.react-toast-notifications__toast', 5);
-    I.see('Login failed.', { css: '.react-toast-notifications__toast' });
-  });
+  I.amOnPage('/');
+  I.waitForElement('input');
+  I.fillField('Authorization token', '0101010101');
+  I.click('Login');
+  I.waitForElement('.react-toast-notifications__toast', 5);
+  I.see('Login failed.', { css: '.react-toast-notifications__toast' });
 });
