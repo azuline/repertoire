a6d4a70faac0d7e9561359ab6a29942fbc0cceaa
blissful <blissful@sunsetglow.net>
2021-04-04 14:16:23 -0400

    update color

--- frontend/src/components/loading/FullPage.tsx
+++ frontend/src/components/loading/FullPage.tsx
@@ -6,7 +6,7 @@ import { colors } from '~/colors';
 export const FullPageLoading: React.FC = () => (
   <div tw="full flex items-center justify-center">
     <ReactLoading
-      color={colors.primary[400]}
+      color={colors.primary[500]}
       height={120}
       type="spinningBubbles"
       width={120}
