a4f332a1369392ad8e5502122c365930dc3fbb5f
blissful <blissful@sunsetglow.net>
2021-04-04 16:10:39 -0400

    Revert "del token command"
    This reverts commit 1f5094fe018364a60c8361960a4d355d9ef437df.

---
+++ backend/src/cli/token.py
@@ -0,0 +1,21 @@
+import click
+
+from src.cli.commands import commands, shared_options
+from src.library import user
+from src.util import database
+
+
+@commands.command()
+@shared_options
+def token():
+    """Generate an authorization token."""
+    # Currently, we only support a single user.
+    with database() as conn:
+        if usr := user.from_id(1, conn):
+            token = user.new_token(usr, conn)
+        else:
+            _, token = user.create("admin", conn)
+
+        conn.commit()
+
+    click.echo(f"Generated new authorization token: {token.hex()}")
