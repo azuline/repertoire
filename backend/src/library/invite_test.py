2c86e2568e3d8a1ee25e8c37ad0b445e170b13c1
blissful <blissful@sunsetglow.net>
2021-04-17 15:56:57 -0400

    Fix invites GraphQL backend

--- backend/src/library/invite_test.py
+++ backend/src/library/invite_test.py
@@ -25,6 +25,65 @@ def test_from_code_failure(db: Connection):
     assert invite.from_code(b"0" * invite.INVITE_LENGTH, db) is None
 
 
+def test_search_all(factory: Factory, db: Connection):
+    invs = {factory.invite(conn=db) for _ in range(5)}
+    # Remove the favorites invite for an easy comparison.
+    out = set(invite.search(db))
+    assert invs == out
+
+
+def test_search_created_by(factory: Factory, db: Connection):
+    usr, _ = factory.user(conn=db)
+    invs_from_user = [factory.invite(by_user=usr, conn=db) for _ in range(2)]
+
+    for _ in range(3):
+        factory.invite(conn=db)
+
+    invs = invite.search(db, created_by=usr.id)
+    assert set(invs) == set(invs_from_user)
+
+
+def test_search_expired(factory: Factory, db: Connection):
+    invs = [factory.invite(conn=db) for _ in range(3)]
+
+    # Expire the third invite.
+    db.execute(
+        """
+        UPDATE system__invites
+        SET created_at = DATETIME(CURRENT_TIMESTAMP, '-2 DAYS')
+        WHERE id = ?
+        """,
+        (invs[2].id,),
+    )
+
+    out = invite.search(db, include_expired=True)
+    assert set(i.id for i in out) == set(i.id for i in invs)
+
+    out = invite.search(db, include_expired=False)
+    assert set(out) == set(invs[:2])
+
+
+def test_search_page(factory: Factory, db: Connection):
+    for _ in range(5):
+        factory.invite(conn=db)
+
+    i1 = invite.search(db, page=1, per_page=1)[0]
+    i2 = invite.search(db, page=2, per_page=1)[0]
+    assert i1 != i2
+
+
+def test_search_per_page(factory: Factory, db: Connection):
+    invs = [factory.invite(conn=db) for _ in range(5)]
+    invs = invite.search(db, page=1, per_page=2)
+    assert len(invs) == 2
+
+
+def test_count_all(factory: Factory, db: Connection):
+    invs = [factory.invite(conn=db) for _ in range(5)]
+    count = invite.count(db)
+    assert count == len(invs)
+
+
 def test_create(factory: Factory, db: Connection):
     usr, _ = factory.user(conn=db)
     inv = invite.create(by_user=usr, conn=db)
