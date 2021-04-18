572ae890aad4e11d00599b2caa65a11133e76631
blissful <blissful@sunsetglow.net>
2021-04-18 12:26:15 -0400

    move src/migrations_test to migrations directory

--- backend/src/migrations/database_test.py
+++ backend/src/migrations/database_test.py
@@ -1,5 +1,8 @@
 import sqlite3
 
+from yoyo import get_backend, read_migrations
+
+from src.constants import Constants
 from src.util import freeze_database_time
 
 from .database import run_database_migrations
@@ -15,3 +18,19 @@ def test_run_database_migrations(isolated_dir):
         freeze_database_time(conn)
         cursor = conn.execute("SELECT 1 FROM _yoyo_version")
         assert len(cursor.fetchall()) > 0
+
+
+def test_migrations(isolated_dir):
+    """
+    Test that, for each migration, the up -> down -> up path doesn't
+    cause an error. Basically, ladder our way up through the migration
+    chain.
+    """
+    cons = Constants()
+    backend = get_backend(f"sqlite:///{isolated_dir / 'db.sqlite3'}")
+    migrations = read_migrations(str(cons.migrations_path))
+
+    for mig in migrations:
+        backend.apply_one(mig)
+        backend.rollback_one(mig)
+        backend.apply_one(mig)
