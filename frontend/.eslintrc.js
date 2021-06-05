2544d3d51a6871ffe09114e50b0f3874057f3e33
blissful <blissful@sunsetglow.net>
2021-06-05 15:27:19 -0400

    fix eslint cfg

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
     // This must be the last extension.
     'prettier/react',
+    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
   plugins: [
